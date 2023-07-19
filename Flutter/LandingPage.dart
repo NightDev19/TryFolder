7aa44b8ec2498d3e2691d399cac2d1c96b50193e
NightDev19 <sherwintajan143@gmail.com>
2023-07-19 09:05:57 +0800

    Updated Landing Page And Add New C++ File

--- Flutter/LandingPage.dart
+++ Flutter/LandingPage.dart
@@ -12,21 +12,12 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(),
-      initialRoute: '/',
-      routes: {
-        // When navigating to the "/" route, build the FirstScreen widget.
-        '/': (context) => const MyHomePage(),
-        // When navigating to the "/second" route, build the SecondScreen widget.
-        '/second': (context) => const AboutMe(),
-      },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({
-    Key? key,
-  }) : super(key: key);
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -50,7 +41,8 @@ class _MyHomePageState extends State<MyHomePage> {
           child: const Text('Open route'),
           onPressed: () {
             // Navigate to second route when tapped.
-            Navigator.pushNamed(context, '/second');
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const AboutMe()));
           },
         ),
       ),
@@ -69,7 +61,8 @@ class _AboutMe extends State<AboutMe> {
         child: ElevatedButton(
           onPressed: () {
             // Navigate back to first route when tapped.
-            Navigator.pushNamed(context,"/");
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const MyHomePage()));
           },
           child: const Text('Go back!'),
         ),
